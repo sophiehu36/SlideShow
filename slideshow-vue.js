720ed9f76c52747d9b527fef9c1695003630224e
sophiehu36 <sophiehu36@163.com>
2021-04-16 13:39:30 +0800

    update

--- slideshow-vue.js
+++ slideshow-vue.js
@@ -13,59 +13,31 @@ new Vue({
 	// },
 	methods: {
 		previousImg: function () {
-			this.number = ++this.number % 4;
+			this.number = (--this.number + 4) % 4;
 		},
 		nextImg: function () {
-			this.number = (--this.number + 4) % 4;
+			this.number = ++this.number % 4;
 		},
 		// method没有缓存，并且html里面要调用这个方法imgSrc()
 		// <img id="slide-image" v-bind:src="imgSrc()" alt="" />
 		imgSrc: function () {
 			return "images/" + this.number + ".jpg";
 		},
+		changeSrc1: function () {
+			this.number = 0;
+		},
+		changeSrc2: function () {
+			this.number = 1;
+		},
+		changeSrc3: function () {
+			this.number = 2;
+		},
+		changeSrc4: function () {
+			this.number = 3;
+		},
 	},
 });
 
-// //点击小圆点切换图片
-// const bindEventIndicator = function () {
-// 	//选中小圆点的父元素
-// 	const indiContainer = e(".slide-indicators");
-// 	//利用事件委托，绑定在父元素上，获取事件
-// 	bindEvent(indiContainer, "mouseover", function (event) {
-// 		const target = event.target;
-// 		//判断事件对象是否为小圆点
-// 		if (target.classList.contains("slide-indi")) {
-// 			//获取当前的小圆点的下标
-// 			const index = target.dataset.indi;
-// 			//选中图片框
-// 			const slide = e(".slide-container");
-// 			//设置当前轮播图片下标为小圆点下标
-// 			slide.dataset.active = index;
-// 			// log(index)
-// 			//切换显示图片
-// 			addClass(index, ".slide-image", "img-active");
-// 			//切换对应下标的小圆点效果
-// 			addClass(index, ".slide-indi", "white");
-// 		}
-// 	});
-// };
-
-// //切换图片
-// const autoPlay = function () {
-// 	//选中slide-container
-// 	const slide = e(".slide-container");
-// 	//得到当前总图片数和当前图片下标
-// 	const numberOfImgs = parseInt(slide.dataset.imgs);
-// 	const activeIndex = parseInt(slide.dataset.active);
-// 	//计算出下一张图片下标
-// 	let nextIndex = (activeIndex + 1) % numberOfImgs;
-// 	//设置当前轮播图片下标
-// 	slide.dataset.active = nextIndex;
-// 	//切换显示图片
-// 	addClass(nextIndex, ".slide-image", "img-active");
-// 	//切换对应下标的小圆点效果
-// 	addClass(nextIndex, ".slide-indi", "white");
-// };
 
 // //设置自动切换图片定时器
 // //设置鼠标移动到图片区域时清除自动播放定时器
